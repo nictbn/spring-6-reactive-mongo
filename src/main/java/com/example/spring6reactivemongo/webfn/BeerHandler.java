c73e6b02fe30a3b8085c2eea618b2422ba9a8d6d
Nicu Taban <nictbndeveloper@gmail.com>
2023-12-26 15:43:48 +0200

    Handles the beerStyle query parameter

--- src/main/java/com/example/spring6reactivemongo/webfn/BeerHandler.java
+++ src/main/java/com/example/spring6reactivemongo/webfn/BeerHandler.java
@@ -13,6 +13,7 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.server.ServerWebInputException;
 import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import static com.example.spring6reactivemongo.webfn.BeerRouter.BEER_PATH_ID;
@@ -24,7 +25,13 @@ public class BeerHandler {
     private final Validator validator;
 
     public Mono<ServerResponse> listBeers(ServerRequest request) {
-        return ServerResponse.ok().body(beerService.listBeers(), BeerDto.class);
+        Flux<BeerDto> flux;
+        if (request.queryParam("beerStyle").isPresent()) {
+            flux = beerService.findByBeerStyle(request.queryParam("beerStyle").get());
+        } else {
+            flux = beerService.listBeers();
+        }
+        return ServerResponse.ok().body(flux, BeerDto.class);
     }
 
     public Mono<ServerResponse> getBeerById(ServerRequest request) {
